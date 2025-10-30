e507e82050046f991eb5d10f6c87a9b501e593fe
TTruong12 <ttruong2@terpmail.umd.edu>
2025-10-30 12:06:20 -0400

    Updated db_manager
    added __itit__ and __repr__
    
    changed class methods to instance methods

--- src/db_manager.py
+++ src/db_manager.py
@@ -19,59 +19,75 @@ class db_manager:
     Manages GET calls to food central API
     Requires: requests library
     """
-
-    __USDA_URL = "https://api.nal.usda.gov/fdc/v1" #URL for the food central database
-    __usda_key = "DEMO_KEY"  # Replace with your USDA key
-
-        
+    def __init__(self, url, key, default_key):
+        self.__url = url
+        self.__key = key
+        self.__default_key = default_key 
 
     @classmethod
-    def USDA_URL(cls):
-        return cls.__USDA_URL
-    
-    @classmethod
-    def usda_key(cls):
-        return cls.__usda_key
+    def foodcentral(cls, key = "DEMO_KEY"):
+        return cls("https://api.nal.usda.gov/fdc/v1", key, "DEMO_KEY")
+
+    def __repr__(self):
+        return f"db_manager | URL: {self.url} API Key: {self.key}"    
     
 
-    @classmethod
-    def usda_key(cls, key = "DEMO_KEY"):
-        """Sets api key to key parameter if valid key"""
+    @property
+    def url(self):
+        return self.__url
+    
+    @property
+    def key(self):
+        return self.__key
+    
+    @property
+    def default_key(self):
+        return self.__default_key
+
+    @key.setter
+    def key(self, key = "DEMO_KEY"):
+        """Sets api key to key parameter if valid key. Resets key to DEMO_KEY if key is unspecified."""
+        if key == "DEMO_KEY" or key == "":
+            key = self.prompt_key()
         print("Validating...")
-        test_url = f"{cls.__USDA_URL}/food/534358?api_key={key}" 
+
+        test_url = f"{self.url}/food/534358?api_key={key}" 
         
         response = requests.get(test_url)
         if (response.status_code == 200):
-            cls.__usda_key = key
+            self.__key = key
             print(f"Success. Status Code: {response.status_code}")
         else:
             print(f"Error. Status Code: {response.status_code}\nURL: {test_url}")
 
 
-    @classmethod
-    def get_food_item(cls, upc):
+    
+    def get_food_item(self, upc):
         """Given the universal product code, returns dictionary of details of that item"""
-        url = f"{cls.__USDA_URL}/food/{upc}?api_key={cls.__usda_key}"
-
+        request_url = f"{self.url}/food/{upc}?api_key={self.key}"
         print("Retrieving...")
-        
-        response = requests.get(url)
+        response = requests.get(request_url)
 
         if response.status_code == 200:
             food_data = response.json()
             return food_data
         else:
-            print(f"Failed to retrieve data {response.status_code}")
+            print(f"Failed to retrieve data {response.status_code}\nURL: {request_url}")
 
 
-
-    def prompt_key() -> str:
+    
+    def prompt_key(self) -> str:
         """Prompts user for api key"""
-        test_key = input("Enter USDA Food Central API key: ")
-        return test_key
+        test_key = input("Enter API key (Press enter to use default): ")
+        if test_key !="":
+            return test_key
+        else:
+            return self.default_key
 
 #tests
-#db_manager.usda_key(db_manager.prompt_key())
-#print(db_manager.get_food_item(534358))
+fc_db = db_manager.foodcentral()
+print(repr(fc_db))
 
-#print(db_manager.USDA_URL())
+#print(fc_db.url)
+#print(fc_db.get_food_item(534358))
+
